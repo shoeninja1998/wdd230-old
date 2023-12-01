a65f30de4308c1f7f851d7b3cc45572489a81ffc
shoeninja1998 <moerani@byui.edu>
2023-11-30 21:41:32 -0700

    Added Dark Mode

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,65 @@
+const url = './data/members.json';
+
+// Adds members to HTML
+function displayMembers(members){
+    const cards = document.querySelector('#members');
+        // Iterates through each member in JSON list
+        members.forEach((member) =>{
+            let section = document.createElement('section')
+            // section.classList.add('directoryCard');
+            
+            // Card HTML format
+            let sectionHTML = `
+            <img src="${member.imageFile}" alt="${member.name}"></img>
+            <h2>${member.name}</h2>
+            <h3>${member.membershipLevel}</h3>
+            <p>${member.address}</p>
+            <p>${member.phoneNumber}</p>
+            <a href="${member.websiteURL}">Website</a>`;
+        
+        // Adds format to the cards
+        section.innerHTML = sectionHTML;
+        cards.appendChild(section);
+    });
+}
+
+// Grabs member data from JSON file
+async function getMemberData(){
+    const response = await fetch(url)
+    if (response.ok){
+        const data = await response.json();
+        displayMembers(data.members)
+    }
+}
+
+getMemberData();
+
+// Grid & List
+const gridButton = document.getElementById('displayGrid');
+const listButton = document.getElementById('displayList');
+const container = document.querySelector('#members')
+
+gridButton.addEventListener('click', ()=>{
+    // Checks if grid is active
+    if(!gridButton.classList.contains('active')){
+        
+        gridButton.classList.replace('inactive','active')
+
+        listButton.classList.replace('active','inactive')
+
+        container.classList.replace('list','grid')
+    }
+    
+});
+
+listButton.addEventListener('click', () => {
+    // Checks if list is active
+    if(!listButton.classList.contains('active')){
+        
+        listButton.classList.replace('inactive','active');
+
+        gridButton.classList.replace('active','inactive');
+        
+        container.classList.replace('grid','list');
+    }
+});
